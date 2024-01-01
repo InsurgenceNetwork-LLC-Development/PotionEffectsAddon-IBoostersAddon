3a0a9e7db189dcc6fb9ca029d6011ad8eb859474
iHxtch <84823076+iHxtch@users.noreply.github.com>
2024-01-01 05:24:32 -0500

    version update

--- src/main/java/com/insurgencedev/potioneffectsaddon/PotionEffectsAddon.java
+++ src/main/java/com/insurgencedev/potioneffectsaddon/PotionEffectsAddon.java
@@ -7,7 +7,7 @@ import org.insurgencedev.insurgenceboosters.api.addon.IBoostersAddon;
 import org.insurgencedev.insurgenceboosters.api.addon.InsurgenceBoostersAddon;
 
 @Getter
-@IBoostersAddon(name = "PotionEffects", version = "1.0.0", author = "Insurgence Dev Team", description = "Apply Potion Effects")
+@IBoostersAddon(name = "PotionEffects", version = "1.0.1", author = "Insurgence Dev Team", description = "Apply Potion Effects")
 public class PotionEffectsAddon extends InsurgenceBoostersAddon {
 
     private static PotionEffectsAddon instance;
